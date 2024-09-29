e5f73145fa8b4bed4bd755e4ea0110631d415f77
Rony Miranda <ronyemb@gmail.com>
2024-09-28 22:16:18 -0500

    commit de ultimos cambios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
+import EditarEmpleado from "./empleados/EditarEmpleado";
+
 function App() {
   return (
-    <div className="container text-center">
-      <h3>Sistema de Recursos Humanos</h3>
+    <div className="container">
+      <BrowserRouter>
+        <Navegacion />
+        <Routes>
+          <Route exact path="/" element={<ListadoEmpleados />} />
+          <Route exact path="/agregar" element={<AgregarEmpleado />} />
+          <Route exact path="/editar/:id" element={<EditarEmpleado />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
